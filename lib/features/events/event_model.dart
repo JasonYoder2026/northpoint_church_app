9ba477fca7866085f80d4169e667061d0ea7057a
Jason Yoder <jason.yoder2@bsu.edu>
2026-02-20 09:28:45 -0500

    Removes ()

--- lib/features/events/event_model.dart
+++ lib/features/events/event_model.dart
@@ -10,7 +10,6 @@ class Event {
   final bool allDay;
   final String? location;
 
-
   Event({
     required this.id,
     required this.title,
@@ -20,17 +19,19 @@ class Event {
     this.startAt,
     this.endAt,
     required this.allDay,
-    this.location
+    this.location,
   });
 
   factory Event.fromMap(Map<String, dynamic> map) {
     return Event(
       id: map['id'].toString(),
-      title: map['title'] ?? '',
+      title: (map['title'] ?? '').replaceAll(RegExp(r'\s*\(.*?\)'), ''),
       summary: map['summary'],
       imageUrl: map['image_url'],
       registrationUrl: map['registration_url'],
-      startAt: map['starts_at'] != null ? DateTime.parse(map['starts_at']) : null,
+      startAt: map['starts_at'] != null
+          ? DateTime.parse(map['starts_at'])
+          : null,
       endAt: map['ends_at'] != null ? DateTime.parse(map['ends_at']) : null,
       allDay: map['all_day'] == true,
       location: map['location'],
